decd8bf88ada445a39136d54a8063994a7fc836b
williamzhou0812 <54008865+williamzhou0812@users.noreply.github.com>
2019-12-11 13:22:25 +1100

    part two update

--- main.js
+++ main.js
@@ -7,13 +7,61 @@ const masses = document.querySelector('pre').innerText.split(/\r?\n/);
 // console.log(masses);
 
 
-const calcuateFuel  =  (mass) => {
-  return Math.floor(mass / 3) - 2 > 0 ? Math.floor(mass / 3) - 2 : 0;
+// const calcuateFuel  =  (mass) => {
+//   return Math.floor(mass / 3) - 2 > 0 ? Math.floor(mass / 3) - 2 : 0;
+// }
+
+
+// const requireFuel = (num) => {
+//   let _calcau; 
+//   _calcau = num;
+//   let requireFuel = Math.floor(_calcau / 3) - 2 > 0 ? Math.floor(_calcau / 3) - 2 : 0;
+//   while (_calcau > 0) {
+//     return  calcuateFuel(requireFuel) + _calcau;
+//   }
+
+// }
+
+
+// console.log(calcuateFuel(1969))
+
+// console.log(requireFuel((calcuateFuel(1969))));
+
+
+// PART TWO 
+const calculateMoreFuel = (input) => {
+  const calculateUntilNegativeFuelRequired = (moduleMass) => {
+    const calculateFuel = (input) => {
+      return (Math.floor(parseInt(input) / 3)) - 2;
+    }; 
+
+    let temp = calculateFuel(moduleMass);
+    
+    let totalTempFuel = 0;
+
+    while (temp > 0) {
+      totalTempFuel += temp;
+      temp = calculateFuel(temp);
+    }
+
+    return totalTempFuel;
+  }
+
+  let fuel = masses.map(x => {
+      return calculateUntilNegativeFuelRequired(x);
+    })
+    .reduce((totalFuel, currentCalculatedFuel) => {
+      return totalFuel += currentCalculatedFuel;
+    }, 0);
+  
+  return fuel;
 }
 
-const calcuateTotalFuel = (masses) => { 
-  return masses.map(mass => calcuateFuel(mass)).reduce((total, fuel) => 
-          total + fuel );
-}
 
-console.log(`Total Fuel: ${calcuateTotalFuel(masses)}`);
+
+//   const calcuateTotalFuel = (masses) => { 
+//     return masses.map(mass => calcuateFuel(mass)).reduce((total, fuel) => 
+//             total + fuel );
+//   }
+
+console.log(calculateMoreFuel())
